ec69cb7bec77455e32b1436d1a0d15dd8a7b9da6
Aaron Huynh <000473817@student.vcc.ca>
2024-07-05 16:46:20 -0700

    Missing Number Solution

--- MissingNumber.js
+++ MissingNumber.js
@@ -2,10 +2,11 @@ let array1 = [5, 4 ,1, 2];
 let array2 = [9, 5, 3, 2, 6, 1, 7, 8, 10]
 
 function findMissingNumber(array, n) {
+    // find the expected sum given n.
     const expectedSum = n*(n + 1)/2
-
+    //find actual sum of array
     const trueSum = array.reduce((sum, num) => sum + num, 0)
-
+    //expected sum - actual sum == missing number
     return expectedSum - trueSum
 }
 
